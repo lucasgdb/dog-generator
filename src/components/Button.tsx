ad6c28482611a2402a4677a6dfdeb852e0754dfb
lucasgdb <lucasgdbittencourt@gmail.com>
2023-01-25 13:11:35 -0300

    fix: button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,8 +11,6 @@ export function Button({ className, ...props }: Props) {
         'inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-900 hover:shadow-lg disabled:opacity-30 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out',
         className
       )}
-    >
-      Gerar cachorrinho
-    </button>
+    />
   );
 }
